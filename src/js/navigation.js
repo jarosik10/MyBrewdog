c2ffa7f869c4b631fcb614c924c2c3f013a6b8f1
jarosik10 <jarosik10@interia.pl>
2020-02-23 20:10:35 +0100

    Change class names of elements in header
    Nested elements had wrong class names.

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menu__list__item');
+const menuItems = document.querySelectorAll('.menu__item');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger--active');
