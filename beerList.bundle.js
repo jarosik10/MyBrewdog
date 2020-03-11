dcd316a5182dc144a54edfc46e4f65f89340e0ff
jarosik10 <jarosik10@interia.pl>
2020-03-11 13:58:39 +0100

    Update 2020-03-11T12:58:36.222Z

--- beerList.bundle.js
+++ beerList.bundle.js
@@ -356,19 +356,45 @@ function getQueryVariable(variable) {
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const menuItems = document.querySelectorAll('.menu__item');
+const navigation = document.querySelector('.navigation');
+let isMobileMenuOpen = false;
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger--active');
     menu.classList.toggle('menu--active');
     document.body.classList.toggle('scroll-lock')
-}, false);
+    if (!isMobileMenuOpen) {
+        document.addEventListener('keydown', keepFocusInside);
+    } else {
+        document.removeEventListener('keydown', keepFocusInside);
+    }
+    isMobileMenuOpen = !isMobileMenuOpen;
+});
 
 menuItems.forEach(item => item.addEventListener('click', () => {
     hamburger.classList.remove('hamburger--active');
     menu.classList.remove('menu--active');
     document.body.classList.remove('scroll-lock')
-
+    if (isMobileMenuOpen) {
+        document.removeEventListener('keydown', keepFocusInside);
+        isMobileMenuOpen = false;
+    }
 }));
+
+const keepFocusInside = (event) => {
+    if (event.keyCode == 9) {
+        const focusable = navigation.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+        const focusedIndex = Array.prototype.indexOf.call(focusable, document.activeElement);
+        if (!event.shiftKey && focusedIndex == focusable.length - 1) {
+            focusable[0].focus();
+            event.preventDefault();
+        }
+        else if (event.shiftKey && focusedIndex == 0) {
+            focusable[focusable.length - 1].focus();
+            event.preventDefault();
+        }
+    }
+}
 
 
 /***/ }),
